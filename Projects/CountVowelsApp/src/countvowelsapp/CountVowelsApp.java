9337150c4c752aa562cc27229f87794b5c59941d
Michael O Brien <mikeyob92@hotmail.com>
2013-12-12 13:36:44 +0000

    Added code to take input from the console Added code to iterate through the input and count the number of vowels Added code to print the number of vowels back to the console

--- Projects/CountVowelsApp/src/countvowelsapp/CountVowelsApp.java
+++ Projects/CountVowelsApp/src/countvowelsapp/CountVowelsApp.java
@@ -1,19 +1,48 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This application takes in a string from the console then checks counts the
+ * number of vowels and then prints it back to the console
  */
 package countvowelsapp;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Mikey
+ * @author Michael O Brien | Gr1dm4st3r
  */
 public class CountVowelsApp {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Please enter a String you wish the vowels to be counted: ");
+        
+        Scanner inputScanner = new Scanner(System.in);
+        String originalString = inputScanner.nextLine();
+        
+        char a = 'a';
+        char e = 'e';
+        char i = 'i';
+        char o = 'o';
+        char u = 'u';
+        
+        int numberOfVowels = 0;
+        
+        for (int iter = 0; iter < originalString.length(); iter++){
+            if(originalString.charAt(iter) == a){
+                numberOfVowels++;
+            }
+            else if(originalString.charAt(iter) == e){
+                numberOfVowels++;
+            }
+            else if(originalString.charAt(iter) == i){
+                numberOfVowels++;
+            }
+            else if(originalString.charAt(iter) == o){
+                numberOfVowels++;
+            }
+            else if(originalString.charAt(iter) == u){
+                numberOfVowels++;
+            }
+        }
+        
+        System.out.println("Number of vowels are: "+numberOfVowels);
+        
     }
 }
