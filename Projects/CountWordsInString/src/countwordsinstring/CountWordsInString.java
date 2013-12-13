e850c4de2a54a71d2e638e9f5b3bf6df16d43cfc
Michael O Brien <mikeyob92@hotmail.com>
2013-12-13 20:16:42 +0000

    Added code to the CountWordsInString Class to take input from the command line Added a for loop to iterate through the string and look for blank spaces which indicate the end of a word and add it to the words variable Added code to output the number of words to the console

--- Projects/CountWordsInString/src/countwordsinstring/CountWordsInString.java
+++ Projects/CountWordsInString/src/countwordsinstring/CountWordsInString.java
@@ -1,19 +1,34 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This application takes in a String from the console counts the number of
+ * words and then outputs the amount of words back out to the console
  */
 package countwordsinstring;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Mikey
+ * @author Michael O Brien | Gr1dm4st3r
  */
 public class CountWordsInString {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Please enter a sentance: ");
+        
+        Scanner inputScanner = new Scanner(System.in);
+        String originalString = inputScanner.nextLine();
+        
+        originalString = originalString.trim();
+        
+        
+        int words = 1;
+        for (int i=0;i<=originalString.length()-1;i++)
+        {
+            if (originalString.charAt(i) == ' ' && originalString.charAt(i+1)!=' ')
+            {
+                words++;
+            }
+        }
+        
+        System.out.println("The number of words in the string you have entered is: "+words);
+        
     }
 }
